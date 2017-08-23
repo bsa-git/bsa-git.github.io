2d74e0c6b6c84101fa51997cfebc476bb7783529
bsa-git <bsa2657@yandex.ru>
2017-08-23 14:47:26 +0300

    Update project

--- _nuxt/23.nuxt.bundle.e50c65b8a814033ee5bc.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([23],{1306:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement;return(n._self._c||e)("nuxt")},staticRenderFns:[]}},821:function(n,e,t){var r=t(97)(null,t(1306),null,null);n.exports=r.exports}});
